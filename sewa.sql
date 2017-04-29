6ab6d9767216a8b04a42ff84f10a328e782e9dc5
Fiki Maulana <fikimaul@gmail.com>
2017-04-29 09:53:43 +0700

    Update sql

--- sewa.sql
+++ sewa.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 07, 2017 at 12:58 
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sewa`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bayar`
---
-
 CREATE TABLE `bayar` (
   `id_bayar` int(11) NOT NULL,
   `id_sewa` int(11) NOT NULL,
@@ -36,10 +8,6 @@ CREATE TABLE `bayar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
---
--- Table structure for table `jaminan`
---
-
 CREATE TABLE `jaminan` (
   `id_jaminan` int(11) NOT NULL,
   `jenis_jaminan` enum('STNK Motor','KTP') NOT NULL,
@@ -48,9 +16,6 @@ CREATE TABLE `jaminan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
---
--- Table structure for table `mobil`
---
 
 CREATE TABLE `mobil` (
   `id_mobil` int(11) NOT NULL,
@@ -63,9 +28,6 @@ CREATE TABLE `mobil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
---
--- Table structure for table `pengembalian`
---
 
 CREATE TABLE `pengembalian` (
   `id_pengembalian` int(11) NOT NULL,
@@ -74,9 +36,6 @@ CREATE TABLE `pengembalian` (
   `denda` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `penyewa`
---
 
 CREATE TABLE `penyewa` (
   `id_penyewa` int(11) NOT NULL,
@@ -84,11 +43,6 @@ CREATE TABLE `penyewa` (
   `alamat_penyewa` varchar(45) DEFAULT NULL,
   `notlp_penyewa` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `sewa`
---
 
 CREATE TABLE `sewa` (
   `id_sewa` int(11) NOT NULL,
@@ -101,10 +55,6 @@ CREATE TABLE `sewa` (
   `harga_sewa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `sopir`
---
-
 CREATE TABLE `sopir` (
   `id_sopir` int(11) NOT NULL,
   `nama_sopir` varchar(45) DEFAULT NULL,
@@ -113,98 +63,54 @@ CREATE TABLE `sopir` (
   `status_sopir` enum('Disewa','Tidak Disewa') NOT NULL DEFAULT 'Tidak Disewa'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bayar`
---
 ALTER TABLE `bayar`
   ADD PRIMARY KEY (`id_bayar`),
   ADD KEY `id_bayar` (`id_bayar`);
 
---
--- Indexes for table `jaminan`
---
+
 ALTER TABLE `jaminan`
   ADD PRIMARY KEY (`id_jaminan`),
   ADD KEY `id_jaminan` (`id_jaminan`);
 
---
--- Indexes for table `mobil`
---
+
 ALTER TABLE `mobil`
   ADD PRIMARY KEY (`id_mobil`),
   ADD KEY `id_mobil` (`id_mobil`);
 
---
--- Indexes for table `pengembalian`
---
 ALTER TABLE `pengembalian`
   ADD PRIMARY KEY (`id_pengembalian`),
   ADD KEY `id_pengembalian` (`id_pengembalian`);
 
---
--- Indexes for table `penyewa`
---
 ALTER TABLE `penyewa`
   ADD PRIMARY KEY (`id_penyewa`),
   ADD KEY `id_penyewa` (`id_penyewa`);
 
---
--- Indexes for table `sewa`
---
 ALTER TABLE `sewa`
   ADD PRIMARY KEY (`id_sewa`),
   ADD KEY `id_sewa` (`id_sewa`);
 
---
--- Indexes for table `sopir`
---
 ALTER TABLE `sopir`
   ADD PRIMARY KEY (`id_sopir`),
   ADD KEY `id_sopir` (`id_sopir`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `bayar`
---
 ALTER TABLE `bayar`
   MODIFY `id_bayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `jaminan`
---
+
 ALTER TABLE `jaminan`
   MODIFY `id_jaminan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT for table `mobil`
---
+
 ALTER TABLE `mobil`
   MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `pengembalian`
---
+
 ALTER TABLE `pengembalian`
   MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `penyewa`
---
+
 ALTER TABLE `penyewa`
   MODIFY `id_penyewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `sewa`
---
+
 ALTER TABLE `sewa`
   MODIFY `id_sewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
---
--- AUTO_INCREMENT for table `sopir`
---
+
 ALTER TABLE `sopir`
   MODIFY `id_sopir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
